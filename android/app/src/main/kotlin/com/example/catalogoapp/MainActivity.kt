9b5460e9df7e50b4e5f387983b22f035691e750b
Werisu <wellysson35@gmail.com>
2021-06-22 15:38:27 -0300

    agora vai

---
+++ android/app/src/main/kotlin/com/example/catalogoapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.catalogoapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Catálogo',
+      theme: ThemeData(
+        bannerTheme: null,
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
