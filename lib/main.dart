6e9817052742681adbd0c63cba524096bfe3c1fd
Werisu <wellysson35@gmail.com>
2021-06-22 15:56:10 -0300

    Agora Vai
    Signed-off-by: Wellysson Rocha <wellysson35@gmail.com>
    Signed-off-by: Werisu <wellysson35@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Catálogo',
+      title: 'Catálogo Vitual',
       theme: ThemeData(
         bannerTheme: null,
         primarySwatch: Colors.blue,
