33737d679641f21db87e1fa265adc2fcec97c4ff
Werisu <wellysson35@gmail.com>
2021-06-23 02:44:34 -0300

    Firebase, primeiro teste Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
+
+  //FirebaseFirestore.instance.collection('teste').add({'teste':'teste'});
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Catálogo Vitual',
       theme: ThemeData(
-        bannerTheme: null,
         primarySwatch: Colors.blue,
       ),
       home: Container(),
