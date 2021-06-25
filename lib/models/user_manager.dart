af93fa2195ba9c981a90d7add96992d6cd971a4b
Werisu <wellysson35@gmail.com>
2021-06-24 23:14:31 -0300

    Carregar usuário ao abrir o aplicativo

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -4,17 +4,23 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
 class UserManager extends ChangeNotifier{
+
+  UserManager(){
+    _loadCurrentUser();
+  }
   
   final FirebaseAuth auth = FirebaseAuth.instance;
+  late final User user;
 
-  bool loading = false;
+  bool _loading = false;
+  bool get loading => _loading;
   
   Future<void> signIn({
     required UserData user,
     required Function onFail,
     required Function onSuccess
   }) async{
-    setLoading(true);
+    loading = true;
     try {
       // Devoluções = Task de AuthResult com o resultado da operação. Acesso: https://firebase.google.com/docs/reference/android/com/google/firebase/auth/FirebaseAuth#returns_8
       final UserCredential result = await auth.signInWithEmailAndPassword(
@@ -22,17 +28,26 @@ class UserManager extends ChangeNotifier{
           password: user.password,
       );
 
-      await Future.delayed(Duration(seconds: 4));
+      this.user = result.user!;
 
       onSuccess();
     } on FirebaseAuthException catch(e){
       onFail(getErrorString(e.code));
     }
-    setLoading(false);
+    loading = false;
   }
 
-  void setLoading(bool value){
-    loading = value;
+  set loading(bool value){
+    _loading = value;
+    notifyListeners();
+  }
+
+  void _loadCurrentUser() async{
+    final User? currentUser = FirebaseAuth.instance.currentUser;
+    if(currentUser != null){
+      user = currentUser;
+      print("uid: "+user.uid);
+    }
     notifyListeners();
   }
 }
