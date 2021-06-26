7cff855b6c347c5f46cbdbc2ffd7968c92c57c83
Werisu <wellysson35@gmail.com>
2021-06-26 16:49:24 -0300

    Recuperando os Dados no login

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -8,13 +8,15 @@ import 'package:flutter/cupertino.dart';
 class UserManager extends ChangeNotifier{
 
   UserManager(){
+    // entrou no app !!!
+    // chama o _loadCurrentUser
     _loadCurrentUser();
   }
   
   final FirebaseAuth auth = FirebaseAuth.instance;
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  late final User user;
+  UserData user = UserData();
 
   bool _loading = false;
   bool get loading => _loading;
@@ -26,13 +28,16 @@ class UserManager extends ChangeNotifier{
   }) async{
     loading = true;
     try {
+      // fez login Sucesso !!!
       // Devoluções = Task de AuthResult com o resultado da operação. Acesso: https://firebase.google.com/docs/reference/android/com/google/firebase/auth/FirebaseAuth#returns_8
+      // pega usuário do firebase (result)
       final UserCredential result = await auth.signInWithEmailAndPassword(
           email: user.email,
           password: user.password,
       );
 
-      this.user = result.user!;
+      //manda para o _loadCurrentUser(result para _loadCurrentUser)
+      await _loadCurrentUser(firebaseUser: result.user);
 
       onSuccess();
     } on FirebaseAuthException catch(e){
@@ -51,6 +56,7 @@ class UserManager extends ChangeNotifier{
       final UserCredential result = await auth.createUserWithEmailAndPassword(email: user.email, password: user.password);
 
       user.id = result.user!.uid;
+      this.user = user;
 
       await user.saveData();
 
@@ -66,15 +72,22 @@ class UserManager extends ChangeNotifier{
     notifyListeners();
   }
 
-  void _loadCurrentUser() async{
-    final User? currentUser = FirebaseAuth.instance.currentUser;
+  /// _loadCurrentUser(User?): função para carregar usuário ou fazer login
+  /// o user pode ser nulo, ou seja, não estar logado ou pode vim com
+  /// usuário. Se vinher nulo significa que o usuário não está logado no app
+  /// se vinher com dados significa que usuário está logado
+  Future<void> _loadCurrentUser({User? firebaseUser}) async{
+    final User? currentUser = firebaseUser ?? FirebaseAuth.instance.currentUser;
     /// se usuário for diferente de nulo
     if(currentUser != null){
       /// acessando a coleção users, pegando o uid do usuário que está logado no momento
       /// e dando um get para obter p documento
       final DocumentSnapshot docUser = await firestore.collection('users').doc(currentUser.uid).get();
 
+      /// obter os dados do usuário logado
+      user = UserData.fromDocument(docUser);
+      print(user.name);
+      notifyListeners();
     }
-    notifyListeners();
   }
 }
