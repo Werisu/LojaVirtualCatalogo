bcc318caa44abbed8afa82ca25c80b8c33153e7e
Werisu <wellysson35@gmail.com>
2021-06-28 17:49:38 -0300

    Pesquisando produtos pelo nome

--- lib/models/product-manager.dart
+++ lib/models/product-manager.dart
@@ -14,6 +14,29 @@ class ProductManager extends ChangeNotifier{
 
   List<Product> allProducts = [];
 
+  String _search = "";
+
+  String get search => _search;
+
+  set search(String value){
+    _search = value;
+    notifyListeners();
+  }
+
+  List<Product> get filteredProducts {
+    final List<Product> filteredProducts = [];
+
+    if(search.isEmpty){
+      filteredProducts.addAll(allProducts);
+    }else{
+      filteredProducts.addAll(
+          allProducts.where((p) => p.name.toLowerCase().contains(search.toLowerCase()))
+      );
+    }
+
+    return filteredProducts;
+  }
+
   Future<void> _loadAllProducts() async{
     final QuerySnapshot querySnapshot = await firestore.collection('products').get();
 
--- lib/screens/products/products_screen.dart
+++ lib/screens/products/products_screen.dart
@@ -16,8 +16,14 @@ class ProductsScreen extends StatelessWidget {
         actions: [
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: (){
-              showDialog(context: context, builder: (_)=>SearchDialog());
+            onPressed: () async{
+              final search = await showDialog<String>(
+                  context: context,
+                  builder: (_)=>SearchDialog()
+              );
+              if(search != ""){
+                context.read<ProductManager>().search = search!;
+              }
             },
           )
         ],
@@ -26,11 +32,12 @@ class ProductsScreen extends StatelessWidget {
       ),
       body: Consumer<ProductManager>(
         builder: (_, productManager, __){
+          final filteredProducts = productManager.filteredProducts;
           return ListView.builder(
             padding: const EdgeInsets.all(4),
-            itemCount: productManager.allProducts.length,
+            itemCount: filteredProducts.length,
             itemBuilder: (_, index){
-              return ProductListTile(productManager.allProducts[index]);
+              return ProductListTile(filteredProducts[index]);
             },
           );
         },
