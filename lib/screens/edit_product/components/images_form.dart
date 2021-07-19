7eaa21af8d0b573a954243fc69d55b6bf35c8ab5
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2021-07-19 17:51:52 -0300

    Imagens no formulário
    Exibindo imagens no formulário e trabalhando o botão remover localmente sem alteração no firebase

--- lib/screens/edit_product/components/images_form.dart
+++ lib/screens/edit_product/components/images_form.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:catalogoapp/models/product.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +14,38 @@ class ImagesForm extends StatelessWidget {
     return FormField<List<dynamic>>(
       initialValue: product.images,
       builder: (state){
-
+        return AspectRatio(
+          aspectRatio: 1,
+          child: Carousel(
+            images: state.value!.map((image){
+              return Stack(
+                fit: StackFit.expand,
+                children: [
+                  if(image is String)
+                    Image.network(image, fit: BoxFit.cover,)
+                  else
+                    Image.file(image as File, fit: BoxFit.cover),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      icon: Icon(Icons.remove),
+                      color: Colors.red,
+                      onPressed: (){
+                        state.value!.remove(image);
+                        state.didChange(state.value);
+                      },
+                    ),
+                  )
+                ],
+              );
+            }).toList(),
+            dotSize: 4,
+            dotSpacing: 15,
+            dotBgColor: Colors.transparent,
+            dotColor: Theme.of(context).primaryColor,
+            autoplay: false,
+          ),
+        );
       },
     );
   }
